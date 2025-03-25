e01592c9b6892fd45155f2ca6a5347be920536c2
yangbinchen <shucyb@163.com>
2025-03-25 13:55:31 +0800

    chore: 添加数据模块及相关JSON文件
    添加了数据模块的初始化文件，并引入了多个JSON文件，包括PPT模板、函数示例、知识库、React示例和任务计划。这些文件将用于存储和管理项目中的各类数据。

---
+++ data/__init__.py
@@ -0,0 +1,9 @@
+# Data Module
+
+This directory contains:
+- Training datasets
+- Model configurations
+- Intermediate results
+- Evaluation metrics
+
+Each agent has its own subdirectory for data management.
